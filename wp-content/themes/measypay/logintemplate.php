712c2b391be0c8dcde101bd89f9ad5349c6994c3
Rubab Rafique <rr@zerocall.com>
2013-05-13 17:22:27 +0500

    wordpress for measypay site

---
+++ wp-content/themes/measypay/logintemplate.php
@@ -0,0 +1,20 @@
+<?php
+/**
+Template Name: Login Page Template
+ */
+
+get_header(); ?>
+
+<div class="mainbg_Inner">
+  <div class="inner_pg">
+    <!--<div class="inner_pg_left">-->
+      <iframe id="blockrandom" name="iframe" src="http://localhost:86/" width="930px" height="650px" scrolling="auto" frameborder="0" class="wrapper">
+        
+      </iframe>
+   <!-- </div>-->
+    <!-- inner_pg_left -->
+  </div>
+  <!-- inner_bg -->
+</div>
+<!-- mainbg_Inner -->
+<?php get_footer(); ?>
